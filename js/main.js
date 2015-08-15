146198298670c31feffe2f31d346c518e7046c07
Cristiano Acosta <cristiano-acosta@hotmail.com>
2015-08-15 18:28:10 -0300

    Ativação da funcionalidade de leitura em navegadores!

--- js/main.js
+++ js/main.js
@@ -123,7 +123,7 @@ jQuery(document).ready(function ($) {
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
       if (target.length) {
         $('html,body').animate({
-          scrollTop: target.offset().top
+          scrollTop: target.offset().top+10
         }, 1000);
         return false;
       }
